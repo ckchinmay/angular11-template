89d557c2f2c0f49827f1cb529bc0410c81e173c6
ckchinmay <ckchinmaykulkarni@gmail.com>
2021-05-26 02:54:50 +0530

    added PWA capabilities.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigToken, SessionExpirationConfig } from '@app/utility/models/session-expiration-config';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -10,7 +12,13 @@ import { CoreModule } from './core/core.module';
     ],
     imports: [
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [
         {
